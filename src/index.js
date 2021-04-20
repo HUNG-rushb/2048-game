1aeddf790da716ad4962a173d85e114eb888b495
Your Name <you@example.com>
2021-04-20 22:12:31 +0700

    .

--- src/index.js
+++ src/index.js
@@ -1,15 +1,20 @@
 import React from 'react';
-import { render } from 'react-dom';
-import Game from './Game';
-import './index.css';
-import * as serviceWorker from './serviceWorker';
+import ReactDOM from 'react-dom';
+import { Provider } from 'react-redux';
+import { BrowserRouter } from 'react-router-dom';
+import './assets/styles/normalize.scss';
+import './assets/styles/index.scss';
+import Layouts from './layouts';
+import store from './store';
+import registerServiceWorker from './utils/registerServiceWorker';
 
-render(
-  <Game fieldSize={4} />,
+ReactDOM.render(
+  <Provider store={store}>
+    <BrowserRouter>
+      <Layouts />
+    </BrowserRouter>
+  </Provider>,
   document.getElementById('root')
 );
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+registerServiceWorker();
